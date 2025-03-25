6a5fb6ae3cfa9c4e7e94fb8473e352de4828a7ec
claudioaltamura <claudio@claudioaltamura.de>
2025-03-25 09:48:50 +0100

    build(dependencies): upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,40 @@
 plugins {
-	java
-	id("com.diffplug.spotless") version "6.23.2"
-	id("com.github.ben-manes.versions") version "0.50.0"
-	id("org.springframework.boot") version "3.2.0"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("com.diffplug.spotless") version "7.0.2"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "de.claudioltamura"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
 repositories {
-	mavenCentral()
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 dependencies {
-	implementation("com.opencsv:opencsv:5.9")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+    implementation("com.opencsv:opencsv:5.10")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 spotless {
-	java {
-		googleJavaFormat()
-	}
+    java {
+        googleJavaFormat()
+    }
+    kotlinGradle {
+        ktlint()
+    }
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
 }
